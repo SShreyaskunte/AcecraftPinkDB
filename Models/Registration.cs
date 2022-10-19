63912118a9eadbb42a00cc77557e5da48eaae57a
SShreyaskunte <shreyas.kunte@tietoevry.com>
2022-10-19 18:13:04 +0530

    Add project files.

---
+++ Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AcecraftPinkDB.Models
+{
+    public partial class Registration
+    {
+        public int Id { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
+        public string? Useremail { get; set; }
+        public string? Password { get; set; }
+        public string? Confpassword { get; set; }
+        public string? Pname { get; set; }
+        public string? Adno { get; set; }
+    }
+}
